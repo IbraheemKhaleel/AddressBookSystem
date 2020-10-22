9449aa234a72b5f72233a8e23031e48f8a27099c
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 23:30:10 +0530

    Refactored : code for  adding , editing and deleting contact by using name for address book

---
+++ src/AddressBookSystemManage.java
@@ -0,0 +1,41 @@
+
+public class AddressBookSystemManage {
+	
+	private String firstName ;
+	private String lastName ;
+	private String city ;
+	private String zip ;
+	
+	public AddressBookSystemManage(String firstName, String lastName, String city, String zip) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.zip = zip;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AddressBookSystemManage [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city
+				+ ", zip=" + zip + "]\n";
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getZip() {
+		return zip;
+	}
+
+}
