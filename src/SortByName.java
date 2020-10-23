cf0e95fd7f37f1df9d18d464746025b76bd1e104
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-23 23:52:44 +0530

    Added : code to sort people contact details with same city name

---
+++ src/SortByName.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class SortByName implements Comparator<AddressBookSystemManage> {
+	@Override
+    public int compare(AddressBookSystemManage person1, AddressBookSystemManage person2) {
+
+        return (person1.getCity()).compareTo(person2.getCity());
+    }
+
+
+}
